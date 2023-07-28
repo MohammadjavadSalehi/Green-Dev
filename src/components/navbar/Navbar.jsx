55e685a363eadd1cbf88e241b237106d2b4b4ff7
Mohammadjavad Salehi <mjsalehi1381@gmail.com>
2023-07-28 22:19:04 +0330

    fix navbar problem

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./navbar.module.css";
-import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 import { signOut, useSession } from "next-auth/react";
 
 const links = [
@@ -40,25 +39,22 @@ const links = [
 ];
 
 const Navbar = () => {
-  const session = useSession();
-
   return (
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>
         lamamia
       </Link>
       <div className={styles.links}>
-        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
-        {session.status === "authenticated" && (
+        {
           <button className={styles.logout} onClick={signOut}>
             Logout
           </button>
-        )}
+        }
       </div>
     </div>
   );
