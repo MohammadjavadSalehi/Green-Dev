90dff81b0c16924ac1bf15ce26c8d7b3b6b903e0
Mohammadjavad Salehi <83529512+MohammadjavadSalehi@users.noreply.github.com>
2023-10-25 21:37:52 +0330

    delete font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
-import { Inter, Roboto, Poppins } from "next/font/google";
+import { Roboto, Poppins } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import { ThemeProvider } from "@/context/ThemeContext";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Green Dev",
@@ -15,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ThemeProvider>
           <AuthProvider>
             <div className="container">
